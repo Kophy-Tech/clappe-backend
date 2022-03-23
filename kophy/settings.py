62d07aaaff1dcfb9abf97d60a43436fd219a5a73
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2022-03-23 16:31:30 +0100

    Update settings.py

--- kophy/settings.py
+++ kophy/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
     'app',
     'rest_framework',
+    'corsheader',
 ]
 
 MIDDLEWARE = [
@@ -42,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kophy.urls'
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+
