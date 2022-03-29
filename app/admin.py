3ada50f563c80ca0fdca673092b71ee06b2f1d67
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2022-03-29 18:51:29 +0100

    added static files for admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import MyUsers, JWT, Customer
+from .models import Customer, MyUsers, Invoice, ProformaInvoice, Estimate, PurchaseOrder, \
+                    PayInvoice, PayEstimate, PayProforma, PayPurchaseOrder, JWT
 # Register your models here.
 
 
 admin.site.register(MyUsers)
 admin.site.register(JWT)
+admin.site.register(Invoice)
+admin.site.register(ProformaInvoice)
+admin.site.register(Estimate)
+admin.site.register(PurchaseOrder)
+admin.site.register(PayInvoice)
+admin.site.register(PayEstimate)
+admin.site.register(PayProforma)
+admin.site.register(PayPurchaseOrder)
 admin.site.register(Customer)
--- kophy/settings.py
+++ kophy/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kophy.urls'
@@ -125,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+
+# MEDIA Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
